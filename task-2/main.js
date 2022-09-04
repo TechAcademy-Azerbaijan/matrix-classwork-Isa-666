4c624b5929e8fb2dc5a404fc35d7483507632cf0
Isa <110451592+Isa-666@users.noreply.github.com>
2022-09-04 16:27:50 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,4 +1,19 @@
 // Use this array for solution
 let array = [3, 5, -7, 7, 5, -9, -4]
 
-// Write code here. 
+let arr = [];
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    let varmi = false
+    for (let j = i + 1; j < array.length; j++) {
+        const element2 = array[j];
+        if (element == element2) {
+            varmi = true
+            break
+        }
+
+    } if (varmi === false) {
+        arr.push(array[i])
+    }
+} console.log(arr.length); 
