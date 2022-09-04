edbc3dd9378a127243402c7ca9395835593355dc
Isa <110451592+Isa-666@users.noreply.github.com>
2022-09-04 16:30:17 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,4 +1,19 @@
-// Use this array for solution
 let array = [3, 5, -7, 7, 5, -9, -4]
+let arr = [];
 
-// Write code here. 
+for (let i = 0; i < array.length; i++) {
+    let count = 1
+    const element = array[i];
+
+    for (let j = 0; j < array.length; j++) {
+        const element2 = array[j];
+        if (element == element2) {
+            count++;
+
+        }
+    }
+    if (count == 2) {
+        arr.push(element)
+    }
+}
+console.log(arr.join(` `));
