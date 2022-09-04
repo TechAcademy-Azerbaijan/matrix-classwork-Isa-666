b3b20f2cee2a8ad28fa9955eb88d467d8454dff4
Isa <110451592+Isa-666@users.noreply.github.com>
2022-09-04 17:00:41 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,5 +1,6 @@
 // Use this matrix for solution
 
+
 let matrix = [
     [134, 475, 30, 424],
     [303, 151, 419, 235],
@@ -7,4 +8,15 @@ let matrix = [
     [318, 237, 184, 36]
 ];
 
-// Write code here. 
+let sum=0
+let second_sum=0
+for (let i = 0; i < matrix.length; i++) {
+   
+    for (let j = 0; j < matrix[i].length; j++) {
+        
+        sum=matrix[0][0]+matrix[1][1]+matrix[2][2]+matrix[3][3]
+        second_sum=matrix[0][3]+matrix[1][2]+matrix[2][1]+matrix[3][0]
+    }
+    
+}
+console.log(`${sum} ${second_sum}`);
