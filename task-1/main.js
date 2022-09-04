64f78e532dcfac5ef111f9c6315f36b5da48d8a4
Isa <110451592+Isa-666@users.noreply.github.com>
2022-09-04 16:27:22 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,5 +1,27 @@
 // Use this arrays for solution
 let array_1 = [3, 1, 3, 4, 2, 4, 12]
 let array_2 = [4, 15, 43, 1, 15, 1]
+let array_3 = [];
+for (let i = 0; i < array_1.length; i++) {
+    const element1 = array_1[i];
+    let exist = false;
 
-// Write code here. 
+    for (let j = 0; j < array_2.length; j++) {
+        const element2 = array_2[j];
+
+        if (element1 == element2) {
+            exist = true
+            break;
+        }
+ 
+        }
+        if (exist === false) {
+            array_3.push(element1)
+
+
+    }
+
+
+
+}
+console.log(array_3.join(' '));
